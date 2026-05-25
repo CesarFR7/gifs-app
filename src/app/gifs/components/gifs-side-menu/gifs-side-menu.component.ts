31b7f23f34b520a60ca7afdb259cb655b1c5b118
Cesar Flores <136465@udlondres.com>
2026-05-24 23:26:37 -0600

    Creación del side-menu-header

--- src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
+++ src/app/gifs/components/gifs-side-menu/gifs-side-menu.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { SideMenuHeaderComponent } from '../side-menu-header/side-menu-header.component';
-import { SideMenuOptionComponent } from '../side-menu-option/side-menu-option.component';
+import { SideMenuHeaderComponent } from './side-menu-header/side-menu-header.component';
+import { SideMenuOptionComponent } from './side-menu-option/side-menu-option.component';
 
 @Component({
   selector: 'gifs-side-menu',
