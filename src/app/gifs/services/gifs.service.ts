32494d9f8cdc1c3534fcca0ed52ea87c4632d897
Cesar Flores <136465@udlondres.com>
2026-06-02 00:46:43 -0600

    Se creó la carpeta mapper y el archivo gif.mapper.ts
    Se creo el patrón de diseño para transformar los datos de la respuesta HTTP y adaptarlo a la aplicación enviando errores de cambios en la API consultada

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -2,10 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interfaces';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
+
+  trendingGifs = signal<Gif[]>([]);
 
   constructor() {
     this.loadTrendingGifs();
@@ -20,7 +24,9 @@ export class GifService {
         },
       })
       .subscribe((resp) => {
-        console.log(resp);
+        const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        console.log({ gifs });
       });
   }
 }
